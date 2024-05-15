be152a10a6016a79219eb9293e275e6587adb8f5
Finn Ehrlich <finn.ehrlich@dittrich-kollegen.de>
2024-05-15 17:00:24 +0200

    funktionen erweitert

--- flutter_application/lib/home_page.dart
+++ flutter_application/lib/home_page.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:openapi/api.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
-
+  
   final String title;
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
+const List<String> list = <String>['C#', 'Java', 'Pyhon', 'Javscript'];
+
 class _HomePageState extends State<HomePage> {
   _HomePageState();
 
   String _userInput = "";
   String _aiAnswer = "";
-
+  var answer = TextEditingController();
   ChatApi? _api;
+  String currentItem = "C#";
+  var items = ["C#", "Java", "Python", "JavaScript"];
+  
 
   void _setAiAnswer(Message message) {
     setState(() {
-      _aiAnswer = message.message ?? "<no message received>";
+      answer.text = message.message ?? "<no message received>";
+      //UnderLineErrors();
+      //answer.buildTextSpan(context: context, withComposing: )
     });
   }
 
@@ -41,10 +49,47 @@ class _HomePageState extends State<HomePage> {
     _setAiAnswer(response!);
   }
 
+  void AnalyzeCodeError(){
+    _userInput = "Markiere die Fehler im folgenden Programmcode. Gebe mir den Code ohne Erklärung zurück und Schreibe hinter jeden Codeteil der einen Fehler hat mit dem Format 'Code // Error: Errortext.'//'.\n\n$_userInput";
+    _askAI();
+    //UnderLineErrors();
+  }
+
+  void ConvertToOtherLanguage()
+  {
+    _userInput = "Konvertiere folgenden Code NUR in die Sprache $currentItem: \n\n $_userInput";
+    _askAI();
+  }
+
+  void UnderLineErrors()
+  {
+    List<String> codePiece = answer.text.split('\n');
+    String newResponse = "";
+    for(String s in codePiece){
+
+      if(s.contains("// Error"))
+      {
+        Text updated = Text(
+        "$s", 
+        style: TextStyle(
+        decoration: TextDecoration.underline,
+        ),
+        );
+         newResponse += updated.data!;
+
+         answer.value = updated as TextEditingValue;
+      }
+      else newResponse += s;
+      
+    }
+    answer.text = newResponse;
+
+  }
+
   @override
   Widget build(BuildContext context) {
     _api = Provider.of<ChatApi>(context);
-
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -55,15 +100,30 @@ class _HomePageState extends State<HomePage> {
           Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
           Expanded(child: 
           TextButton(
-            onPressed: () => {}, 
-            child: const Text("test")
+            onPressed: AnalyzeCodeError, 
+            child: const Text("Programmcode auf Fehler überprüfen")
           ),
           ),
           Expanded(child:
           TextButton(
-            onPressed: () => {}, 
-            child:const Text("Test2")
+            onPressed: ConvertToOtherLanguage, 
+            child:const Text("Code in andere Sprache Konvertieren")
           ),
+          ),
+          
+          DropdownButton(
+            value: currentItem,
+            items: items.map((String items) { 
+                return DropdownMenuItem( 
+                  value: items, 
+                  child: Text(items), 
+                ); 
+              }).toList(), 
+          onChanged: (String? value){
+            setState((){
+                currentItem = value!;
+            });
+          },
           ),
           Expanded(child:
           TextButton(
@@ -80,10 +140,17 @@ class _HomePageState extends State<HomePage> {
           ],
           ),
           const SizedBox(height: 100),
+          IconButton(onPressed: (){
+            Alignment.centerRight;
+            Clipboard.setData(ClipboardData(text: answer.text));
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Copied")));
+            }, 
+            icon: const Icon(Icons.copy)
+            ),
           Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
           Expanded(child: TextField(
             key: const Key('UserInputTextField'),
-            maxLines: 5,
+            maxLines: 20,
             decoration: const InputDecoration(
               hintText: 'Enter text here',
             ),
@@ -93,8 +160,10 @@ class _HomePageState extends State<HomePage> {
           ),
           ),
           Expanded(child: TextField(
-            key: const Key('UserInputTextField'),
-            maxLines: 5,
+            controller: answer,
+            key: const Key('AiAnswerText'),
+            maxLines: 20,
+            readOnly: true,
             decoration: const InputDecoration(
               hintText: 'Enter text here',
             ),
